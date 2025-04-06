f3178f243728b9039f0fef139ab35a2a344ede18
Abed <robotogeddon@gmail.com>
2025-04-06 10:23:46 -0400

    Initial commit

---
+++ first_name_script.py
@@ -0,0 +1,4 @@
+# Simple Python program to greet the user
+
+first_name = input("Enter your first name: ")  # Get user input
+print(f"Good afternoon, {first_name}! Welcome to Python programming.")  # Print a greeting message
